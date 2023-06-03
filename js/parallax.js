798855165b975505e1b9be40bb3782999a496a7a
ChikinAnton <125805196+ChikinAnton@users.noreply.github.com>
2023-06-03 13:10:14 +0300

    Add files via upload

--- js/parallax.js
+++ js/parallax.js
@@ -106,8 +106,8 @@ if(ScrollTrigger.isTouch !== 1){
     let itemsHTML = gsap.utils.toArray('.technologies-html .technologies__item')
 
 	itemsHTML.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: -75 }, {
-			opacity: 1, x: 0,
+		gsap.fromTo(item, { opacity: 0, y: -225 }, {
+			opacity: 1, y: 0,
 			scrollTrigger: {
 				trigger: item,
 				start: '-850',
@@ -120,9 +120,8 @@ if(ScrollTrigger.isTouch !== 1){
     let itemsCSS = gsap.utils.toArray('.technologies-css .technologies__item')
 
 	itemsCSS.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: 
-            75 }, {
-			opacity: 1, x: 0,
+		gsap.fromTo(item, { opacity: 0, y: -225 }, {
+			opacity: 1, y: 0,
 			scrollTrigger: {
 				trigger: item,
 				start: '-850',
@@ -135,8 +134,8 @@ if(ScrollTrigger.isTouch !== 1){
     let itemsJS = gsap.utils.toArray('.technologies-js .technologies__item')
 
 	itemsJS.forEach(item => {
-		gsap.fromTo(item, { opacity: 0, x: -75 }, {
-			opacity: 1, x: 0,
+		gsap.fromTo(item, { opacity: 0, y: -225 }, {
+			opacity: 1, y: 0,
 			scrollTrigger: {
 				trigger: item,
 				start: '-850',
